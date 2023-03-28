c77c11729f4e71658fe503b294076b371728bb3d
KolojE <Wenkimao927@gmail.com>
2023-03-28 16:33:04 +0800

    categorized by author

--- utils/YTDownload.js
+++ utils/YTDownload.js
@@ -3,45 +3,52 @@ const ffmpeg = require("fluent-ffmpeg");
 const fs = require("fs");
 
 async function YTdownload(link, output, format) {
-    try {
+	try {
 
-        const info = await ytdl.getInfo(link);
-        const videoTitle = info.videoDetails.title.replaceAll('/', '');
-        const fileName = `${output}/${videoTitle}.${format}`;
-        const stream = await ytdl(link, { filter: format === "mp3" ? "audioonly" : "audioandvideo", dlchunks: "8MB" });
+		const info = await ytdl.getInfo(link);
+		const videoTitle = info.videoDetails.title.replaceAll('/', '');
+		const videoAuthor = info.videoDetails.author.name;
+		const outputDir = `${output}/${videoAuthor}`
+		const fileName = `${outputDir}/${videoTitle}.${format}`;
+		const stream = await ytdl(link, { filter: format === "mp3" ? "audioonly" : "audioandvideo", dlchunks: "8MB" });
+		
+		if(!fs.existsSync(outputDir))
+		{
+			fs.mkdirSync(outputDir)
+		}
 
-        if (format === "mp3") {
-            console.log("Download and convert to mp3")
-            ffmpeg(stream)
-                .audioCodec('libmp3lame')
-                .audioBitrate(320)
-                .format('mp3')
-                .on('error', (err) => console.error(err))
-                .on('end', () => console.log('Finished!'))
-                .on("progress", (per) => { console.log(per) })
-                .save(fileName, {
-                    end: true
-                })
-        }
-        else if (format === "mp4") {
-            console.log(fileName)
-            const file = fs.createWriteStream(fileName);
-            stream.on("data", (chunk) => {
-                file.write(chunk);
-            })
-            stream.on("end", () => {
-                file.close();
-            })
+		if (format === "mp3") {
+			console.log("Download and convert to mp3")
+			ffmpeg(stream)
+				.audioCodec('libmp3lame')
+				.audioBitrate(320)
+				.format('mp3')
+				.on('error', (err) => console.error(err))
+				.on('end', () => console.log('Finished!'))
+				.on("progress", (per) => { console.log(per) })
+				.save(fileName, {
+					end: true
+				})
+		}
+		else if (format === "mp4") {
+			console.log(fileName)
+			const file = fs.createWriteStream(fileName);
+			stream.on("data", (chunk) => {
+				file.write(chunk);
+			})
+			stream.on("end", () => {
+				file.close();
+			})
 
-        }
-    }
-    catch (e) {
-        console.error(e);
-    }
+		}
+	}
+	catch (e) {
+		console.error(e);
+	}
 }
 
 
 
 module.exports = {
-    YTdownload: YTdownload,
+	YTdownload: YTdownload,
 };
